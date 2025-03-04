a4fcd72cc34e2276caef603bc7ccfec5feae5f58
Wyatt Core <99035317+WyattCore@users.noreply.github.com>
2025-03-03 20:25:44 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './header.jsx';
+import Home from './Home.jsx';
+import Plans from './Plans.jsx';
+import About from './About.jsx';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import QuestionForm from './QuestionForm.jsx';
+import PLAN_DETAILS from './Plan_details.jsx';
 
-function App() {
+
+function App(){
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <Router>
+      <Header></Header>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/questionform' element={<QuestionForm />} />
+            <Route path="/plans" element={<Plans />} /> 
+            <Route path="/about" element={<About />} />
+            <Route path="/plan_details/:id" element={<PLAN_DETAILS />} />
+        </Routes>
+    </Router>
+    </>
+  
   );
 }
-
-export default App;
+export default App
